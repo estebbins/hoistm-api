9c746306051387bd9eb393e66b616af9e6ab3046
MalcolmKemp <Malokingsley@icloud.com>
2023-02-13 14:15:52 -0500

    'contributors, and labels added'

---
+++ app/models/label.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const User = require('./user')
+const File = require('./file')
+
+
+const labelSchema = new mongoose.Schema({
+
+
+    owner: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+
+	},
+    fileRef: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'File'
+    }],
+    name: {type: String, required: true},
+    color: {type: String, required: true},
+},
+{
+  timestamps: true
+}
+)
+
+module.exports = mongoose.model('Label', labelSchema)
